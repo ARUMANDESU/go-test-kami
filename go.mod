44f6f9b5fb83f65cefb7131b672e24d0177a5a06
ARUMANDESU <zh.arumandes@gmail.com>
2024-08-31 14:50:57 +0500

    reservation service unit tests

--- go.mod
+++ go.mod
@@ -8,16 +8,17 @@ require (
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/samber/slog-chi v1.11.1
 	github.com/segmentio/encoding v0.4.0
 	github.com/stretchr/testify v1.8.4
+	github.com/thejerf/slogassert v0.3.4
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/jackc/pgerrcode v0.0.0-20240316143900-6e2875d9b438 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
@@ -25,6 +26,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/segmentio/asm v1.1.3 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	go.opentelemetry.io/otel v1.19.0 // indirect
 	go.opentelemetry.io/otel/trace v1.19.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
--- internal/service/reservation/reservation_test.go
+++ internal/service/reservation/reservation_test.go
@@ -1,1 +1,325 @@
 package reservation
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"testing"
+	"time"
+
+	"github.com/ARUMANDESU/go-test-kami/internal/domain"
+	"github.com/ARUMANDESU/go-test-kami/internal/service/reservation/mocks"
+	"github.com/ARUMANDESU/go-test-kami/internal/storage"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/thejerf/slogassert"
+)
+
+// suite is a test suite for the reservation service.
+// It contains the common setup and teardown logic for the tests.
+// It also contains the common fields used by the tests.
+type suite struct {
+	logHandler   *slogassert.Handler
+	mockProvider *mocks.Provider
+	mockReserver *mocks.Reserver
+	service      Service
+}
+
+// NewSuite creates a new test suite.
+// It creates a new assert logger handler(slogassert), mock provider, mock reserver, and service.
+func NewSuite(t *testing.T) suite {
+	t.Helper()
+	handler := slogassert.New(t, slog.LevelWarn, nil)
+	logger := slog.New(handler)
+
+	mockProvider := mocks.NewProvider(t)
+	mockReserver := mocks.NewReserver(t)
+
+	service := NewService(logger, mockProvider, mockReserver)
+
+	return suite{
+		logHandler:   handler,
+		mockProvider: mockProvider,
+		mockReserver: mockReserver,
+		service:      service,
+	}
+}
+
+func TestGetRoomReservation_HappyPath(t *testing.T) {
+	tests := []struct {
+		name                      string
+		roomID                    string
+		onGetRoomReservations     []domain.Reservation
+		onGetRoomReservationError error
+	}{
+		{
+			name:                      "get room reservations",
+			roomID:                    uuid.Must(uuid.NewV7()).String(),
+			onGetRoomReservations:     MockReservations(t, time.Now()),
+			onGetRoomReservationError: nil,
+		},
+		{
+			name:                      "no reservations",
+			roomID:                    uuid.Must(uuid.NewV7()).String(),
+			onGetRoomReservations:     []domain.Reservation{},
+			onGetRoomReservationError: storage.ErrNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewSuite(t)
+
+			s.mockProvider.On("GetRoomReservations", mock.Anything, tt.roomID).Return(tt.onGetRoomReservations, tt.onGetRoomReservationError)
+
+			reservations, err := s.service.GetRoomReservations(context.Background(), tt.roomID)
+			require.NoError(t, err)
+
+			require.Equal(t, tt.onGetRoomReservations, reservations)
+
+			s.mockProvider.AssertExpectations(t)
+		})
+	}
+}
+
+func TestGetRoomReservation_FailPath(t *testing.T) {
+	tests := []struct {
+		name                      string
+		roomID                    string
+		onGetRoomReservations     []domain.Reservation
+		onGetRoomReservationError error
+		expectedError             error
+	}{
+		{
+			name:                      "invalide room id: empty",
+			roomID:                    "",
+			onGetRoomReservations:     nil,
+			onGetRoomReservationError: nil,
+			expectedError:             ErrInvalidArgument,
+		},
+		{
+			name:                      "invalid room id, not uuid",
+			roomID:                    "invalid-uuid",
+			onGetRoomReservations:     nil,
+			onGetRoomReservationError: nil,
+			expectedError:             ErrInvalidArgument,
+		},
+		{
+			name:                      "failed to get room reservations",
+			roomID:                    uuid.Must(uuid.NewV7()).String(),
+			onGetRoomReservations:     nil,
+			onGetRoomReservationError: fmt.Errorf("unexpected db error"),
+			expectedError:             ErrInternal,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewSuite(t)
+
+			if !errors.Is(tt.expectedError, ErrInvalidArgument) {
+				s.mockProvider.On("GetRoomReservations", mock.Anything, tt.roomID).Return(tt.onGetRoomReservations, tt.onGetRoomReservationError)
+				defer s.mockProvider.AssertExpectations(t)
+			}
+
+			_, err := s.service.GetRoomReservations(context.Background(), tt.roomID)
+			require.Error(t, err)
+
+			assert.ErrorIs(t, err, tt.expectedError)
+
+		})
+	}
+
+}
+
+func TestReserveRoom_HappyPath(t *testing.T) {
+	timeNow := time.Now()
+
+	tests := []struct {
+		name                       string
+		dto                        domain.ReservationCreateDTO
+		onGetRoomReservations      []domain.Reservation
+		onGetRoomReservationsError error
+	}{
+		{
+			name: "reserve room: no reservations yet",
+			dto: domain.ReservationCreateDTO{
+				RoomID:    uuid.Must(uuid.NewV7()).String(),
+				StartTime: timeNow,
+				EndTime:   timeNow.Add(time.Hour),
+			},
+			onGetRoomReservations:      nil,
+			onGetRoomReservationsError: storage.ErrNotFound,
+		},
+		{
+			name: "reserve room: with reservations",
+			dto: domain.ReservationCreateDTO{
+				RoomID:    uuid.Must(uuid.NewV7()).String(),
+				StartTime: timeNow,
+				EndTime:   timeNow.Add(time.Hour),
+			},
+			onGetRoomReservations: MockReservations(t, timeNow),
+		},
+		{
+			name: "reserve room: with reservations",
+			dto: domain.ReservationCreateDTO{
+				RoomID:    uuid.Must(uuid.NewV7()).String(),
+				StartTime: timeNow.Add(time.Hour * time.Duration(5)),
+				EndTime:   timeNow.Add(time.Hour * time.Duration(6)),
+			},
+			onGetRoomReservations: MockReservations(t, timeNow),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewSuite(t)
+
+			s.mockProvider.On("GetRoomReservations", mock.Anything, tt.dto.RoomID).Return(tt.onGetRoomReservations, tt.onGetRoomReservationsError)
+
+			s.mockReserver.On("ReserveRoom", mock.Anything, tt.dto).Return(domain.Reservation{
+				ID:        uuid.Must(uuid.NewV7()),
+				RoomID:    uuid.FromStringOrNil(tt.dto.RoomID),
+				StartTime: tt.dto.StartTime,
+				EndTime:   tt.dto.EndTime,
+			}, nil)
+
+			reservation, err := s.service.ReserveRoom(context.Background(), tt.dto)
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.dto.RoomID, reservation.RoomID.String())
+			assert.Equal(t, tt.dto.StartTime, reservation.StartTime)
+			assert.Equal(t, tt.dto.EndTime, reservation.EndTime)
+
+			s.mockProvider.AssertExpectations(t)
+			s.mockReserver.AssertExpectations(t)
+		})
+	}
+}
+
+func TestReserveRoom_FailPath(t *testing.T) {
+	timeNow := time.Now()
+
+	tests := []struct {
+		name                       string
+		dto                        domain.ReservationCreateDTO
+		onGetRoomReservations      []domain.Reservation
+		onGetRoomReservationsError error
+		onReserveError             error
+		expectedError              error
+		onReserve                  bool
+	}{
+		{
+			name: "invalid reservation create dto",
+			dto: domain.ReservationCreateDTO{
+				RoomID:    "",
+				StartTime: timeNow,
+				EndTime:   timeNow.Add(time.Hour),
+			},
+			onGetRoomReservations:      nil,
+			onGetRoomReservationsError: nil,
+			onReserveError:             nil,
+			expectedError:              ErrInvalidArgument,
+		},
+		{
+			name: "failed to get room reservations",
+			dto: domain.ReservationCreateDTO{
+				RoomID:    uuid.Must(uuid.NewV7()).String(),
+				StartTime: timeNow,
+				EndTime:   timeNow.Add(time.Hour),
+			},
+			onGetRoomReservations:      nil,
+			onGetRoomReservationsError: fmt.Errorf("unexpected db error"),
+			onReserveError:             nil,
+			expectedError:              ErrInternal,
+		},
+		{
+			name: "reservation conflict",
+			dto: domain.ReservationCreateDTO{
+				RoomID: uuid.Must(uuid.NewV7()).String(),
+				// this time period overlaps with the existing mock reservations
+				StartTime: timeNow.Add(time.Hour),
+				EndTime:   timeNow.Add(time.Hour * 2),
+			},
+			onGetRoomReservations:      MockReservations(t, timeNow),
+			onGetRoomReservationsError: nil,
+			onReserveError:             nil,
+			expectedError:              ErrReservationConflict,
+		},
+		{
+			name: "failed to reserve room, concurrency issue",
+			dto: domain.ReservationCreateDTO{
+				RoomID:    uuid.Must(uuid.NewV7()).String(),
+				StartTime: timeNow,
+				EndTime:   timeNow.Add(time.Hour),
+			},
+			onGetRoomReservations:      MockReservations(t, timeNow),
+			onGetRoomReservationsError: nil,
+			onReserveError:             storage.ErrResevationConflict,
+			expectedError:              ErrReservationConflict,
+			onReserve:                  true,
+		},
+		{
+			name: "failed to reserve room, internal error",
+			dto: domain.ReservationCreateDTO{
+				RoomID:    uuid.Must(uuid.NewV7()).String(),
+				StartTime: timeNow,
+				EndTime:   timeNow.Add(time.Hour),
+			},
+			onGetRoomReservations:      MockReservations(t, timeNow),
+			onGetRoomReservationsError: nil,
+			onReserveError:             fmt.Errorf("unexpected db error"),
+			expectedError:              ErrInternal,
+			onReserve:                  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewSuite(t)
+
+			if !errors.Is(tt.expectedError, ErrInvalidArgument) {
+				s.mockProvider.On("GetRoomReservations", mock.Anything, tt.dto.RoomID).Return(tt.onGetRoomReservations, tt.onGetRoomReservationsError)
+				defer s.mockProvider.AssertExpectations(t)
+
+				if tt.onReserve {
+					s.mockReserver.On("ReserveRoom", mock.Anything, tt.dto).Return(domain.Reservation{}, tt.onReserveError)
+					defer s.mockReserver.AssertExpectations(t)
+				}
+			}
+
+			_, err := s.service.ReserveRoom(context.Background(), tt.dto)
+			require.Error(t, err)
+
+			assert.ErrorIs(t, err, tt.expectedError)
+		})
+	}
+
+}
+
+// MockReservations is a helper function to create a list of reservations for testing purposes
+// It creates 4 reservations with 1 hour difference
+//
+//	now                | now + 1 hour       | now + 2 hours      | now + 3 hours      | now + 4 hours      | now + 5 hours
+//	<====================================================================================================================>
+//	free slot (1 hour) | reserved (1 hour)  | reserved (1 hour)  | reserved (1 hour)  | reserved (1 hour)  | free slots after
+//	free slot : 1 hour (now + 1 hour)
+func MockReservations(t *testing.T, now time.Time) []domain.Reservation {
+	t.Helper()
+
+	var reservations []domain.Reservation
+	for i := 1; i < 5; i++ {
+		reservation := domain.Reservation{
+			ID:        uuid.Must(uuid.NewV7()),
+			RoomID:    uuid.Must(uuid.NewV7()),
+			StartTime: now.Add(time.Hour * time.Duration(i)),
+			EndTime:   now.Add(time.Hour * time.Duration(i+1)),
+		}
+		reservations = append(reservations, reservation)
+	}
+
+	return reservations
+}
